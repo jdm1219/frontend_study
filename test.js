cfdd864c806296ed99b716f9f03467d2c4b852e8
jdm1219 <wjsehaud9413@naver.com>
2019-11-02 17:41:36 +0900

    first commit

--- test.js
+++ test.js
@@ -1,8 +1,7 @@
-// TODO 투두 코맨트 테스트
+// XXX 이게뭔가여...
 const main = (...args) => {
     args.forEach(e => {
         console.log(e);
     });
-}
-// FIXME 픽스미 코맨트 테스트
-main('test1', 'test2', 'test3');
+};
+main("test1", "test2", "test3");
